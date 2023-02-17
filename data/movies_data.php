fd98fe34c86d0f7e40119da46cdd0ca8793810c4
Serena-Dong <serenadong3159@icloud.com>
2023-02-17 18:24:29 +0100

    movies_data

---
+++ data/movies_data.php
@@ -0,0 +1,24 @@
+<?php
+$movies = [
+    [
+        'id' => 1,
+        'title' => 'The Dreamers',
+        'year' => 2003,
+        'director' => 'Bernardo Bertolucci',
+        'genre' => 'Dramatic'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Dead Poet Society',
+        'year' => 1989,
+        'director' => 'Peter Weir',
+        'genre' => 'Romantic'
+    ],
+    [
+        'id' => 3,
+        'title' => 'Oppenheimer',
+        'year' => 2023,
+        'director' => 'Christopher Nolan',
+        'genre' => 'Thriller'
+    ]
+];
