76f41b4e02d5187e707e5583f857b0ed1cf3adbb
Serena-Dong <serenadong3159@icloud.com>
2023-02-17 18:15:02 +0100

    on page

--- index.php
+++ index.php
@@ -6,14 +6,16 @@ class Movie
     public $title;
     public $year;
     public $director;
+    public $genre;
 
 
     // Constructor
-    function __construct($_title, $_year, $_director)
+    function __construct($_title, $_year, $_director, $_genre)
     {
         $this->title = $_title;
         $this->year = $_year;
         $this->director = $_director;
+        $this->genre = $_genre;
     }
 
     // Methods
@@ -29,18 +31,54 @@ class Movie
     }
 }
 
+// Genre Class
+class Genre
+{
+    public $name;
+    function __construct($_name)
+    {
+        $this->name = $_name;
+    }
+}
 // Movie Objects 
-$firstMovie = new Movie('The Dreamers', 2003, 'Bernardo Bertolucci');
-$secondMovie = new Movie('Dead Poet Society', 1989, 'Peter Weir');
-$thirdMovie = new Movie('Oppenheimer', 2023, 'Christopher Nolan');
+$firstMovie = new Movie('The Dreamers', 2003, 'Bernardo Bertolucci', 'Drammatic');
+$secondMovie = new Movie('Dead Poet Society', 1989, 'Peter Weir', 'Romantic');
+$thirdMovie = new Movie('Oppenheimer', 2023, 'Christopher Nolan', 'Thriller');
 
 $firstMovie->current(2003);
 $secondMovie->current(1989);
 $secondMovie->current(2023);
 
+$movies = [
+    [
+        'id' => 1,
+        'title' => 'The Dreamers',
+        'year' => 2003,
+        'director' => 'Bernardo Bertolucci',
+        'genre' => 'Dramatic'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Dead Poet Society',
+        'year' => 1989,
+        'director' => 'Peter Weir',
+        'genre' => 'Romantic'
+    ],
+    [
+        'id' => 3,
+        'title' => 'Oppenheimer',
+        'year' => 2023,
+        'director' => 'Christopher Nolan',
+        'genre' => 'Thriller'
+    ]
+];
 
-var_dump($firstMovie, $secondMovie)
 
+$movies_library = [];
+foreach ($movies as $movie) {
+    $movie_object = new Movie($movie['title'], $movie['year'], $movie['director'], $movie['genre']);
+    $movies_library[] = $movie_object;
+}
 
 ?>
 <!DOCTYPE html>
@@ -51,10 +89,28 @@ var_dump($firstMovie, $secondMovie)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Movie</title>
+
+    <!-- BootStrap -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+    <!-- Css -->
+    <link rel="stylesheet" href="./css/styles.css">
+
 </head>
 
 <body>
-
+    <div class="container">
+        <h1 class="text-center my-5">P H P - M O V I E S</h1>
+        <div class="row justify-content-center ">
+            <?php foreach ($movies_library as $movie) : ?>
+                <div class="col-4 border border-white">
+                    <h3><?= $movie->title ?></h3>
+                    <p><?= $movie->year ?></p>
+                    <p><b><?= $movie->director ?></b></p>
+                    <span><?= $movie->genre ?></span>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </body>
 
 </html>
