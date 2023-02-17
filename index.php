3f07a4e3b0c439c68778d916bc6fa02013b3c4b2
Serena-Dong <serenadong3159@icloud.com>
2023-02-17 17:27:51 +0100

    Movie Class and its functions

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
 <?php
+// Movie Class
+class Movie
+{
+    // Istance Variables
+    public $title;
+    public $year;
+    public $director;
+
+
+    // Constructor
+    function __construct($_title, $_year, $_director)
+    {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->director = $_director;
+    }
+
+    // Methods
+    public function current($_year)
+    {
+        $this->year = $_year;
+        $current_year = date("Y");
+        if ($_year == $current_year) {
+            var_dump('New');
+        } else {
+            var_dump('Old');
+        }
+    }
+}
+
+// Movie Objects 
+$firstMovie = new Movie('The Dreamers', 2003, 'Bernardo Bertolucci');
+$secondMovie = new Movie('Dead Poet Society', 1989, 'Peter Weir');
+
+$firstMovie->current(2003);
+$secondMovie->current(1989);
+
+
+var_dump($firstMovie, $secondMovie)
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
